25c2d49b5baeaca83417ad291f6a3878655c5dc7
JAVA101_01 <JAVA101_01@ACCENTU-JOVUMMU>
2014-11-17 07:13:05 -0600

    Ana

--- CodingtonFinalProject/src/codingtonportal/model/dao/interfaces/EventDAO.java
+++ CodingtonFinalProject/src/codingtonportal/model/dao/interfaces/EventDAO.java
@@ -5,8 +5,42 @@ import java.io.IOException;
 import codingtonportal.model.domain.Event;
 
 public interface EventDAO {
+	/**
+	 * This interface manages the methods relate with the events.
+	 * @param event
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	public boolean insertEvent(Event event) throws IOException, ClassNotFoundException;
+	/**
+	 * The admin inserts events.
+	 * @param event
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	
 	public boolean deleteEvent(Event event) throws IOException, ClassNotFoundException;
+	/**
+	 * The admin deletes events from the list of events.
+	 * @param event
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	
 	public boolean updateEvent(Event event) throws IOException, ClassNotFoundException;
+	/**
+	 * The admin update the event.
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	
 	public boolean selectEvent () throws IOException, ClassNotFoundException;
+	/**
+	 * Select a event.
+	 */
+
 }
