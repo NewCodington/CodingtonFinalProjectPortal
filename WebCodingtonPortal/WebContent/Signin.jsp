f669fd80620acbe287f682631039571dc5216fdf
jorgeNewC <borrem89@hotmail.com>
2014-11-18 12:34:02 +0100

    primer servlet

--- WebCodingtonPortal/WebContent/Signin.jsp
+++ WebCodingtonPortal/WebContent/Signin.jsp
@@ -1,12 +1,49 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
+	  
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<link rel="stylesheet" type="text/css" href="css/codington.css">
+	<title>New Codington Portal</title>
 </head>
-<body>
+
+
+
+<div class="header">
+	<p class="cabecera">New Codington Portal</p>
+	<p class="cabecera2">One of the World's Best Cities 2014</p>
+</div>
+
+
+<div class="nav_log">
+	<form  method = "post" action = "SigninServlet">
+		<p class = "title">
+		Sign In </p>
+		<p class = "title"><br>
+		  <input type="text" id = "uname" name="username" placeholder="Username" required/>
+		  <br>
+	      <input type="password" id = "pass" name="password" placeholder="Password" required/>
+	      <br>
+	  </p>
+	  <p class = "input">
+			<INPUT type="SUBMIT" value="Submit">
+			<INPUT type="SUBMIT" value="Cancel">
+	  </p>
+	</form>
+		
+	<a href="Regist.html" > Register New Visitor </a>
+	
+</div>
+
+
+
+
+
+<div class ="footer">
+Copyright © W3Schools.com
+</div>
 
 </body>
 </html>
--- WebCodingtonPortal/src/codingtonportal/servlet/SigninServlet.java
+++ WebCodingtonPortal/src/codingtonportal/servlet/SigninServlet.java
@@ -1,11 +1,17 @@
 package codingtonportal.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import codingtonportal.model.domain.Visitor;
+
 
 /**
  * Servlet implementation class SigninServlet
@@ -26,14 +32,19 @@ public class SigninServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		RequestDispatcher dispatcher=request.getRequestDispatcher("/Signin.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		Visitor visitor = new Visitor(request.getParameter("username"), request.getParameter("password"));
+		PrintWriter out = response.getWriter();
+		out.println(visitor.getUserName() + " " + visitor.getPassword());
 	}
 
 }
