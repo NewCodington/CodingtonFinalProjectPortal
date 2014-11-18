7c59b2eb3267cd42253d87000e0ba9adb3349c6e
JAVA101_01 <JAVA101_01@ACCENTU-JOVUMMU>
2014-11-18 11:25:57 +0100

    j

--- CodingtonFinalProject/src/codingtonportal/model/dao/interfaces/EventDAO.java
+++ CodingtonFinalProject/src/codingtonportal/model/dao/interfaces/EventDAO.java
@@ -1,6 +1,7 @@
 package codingtonportal.model.dao.interfaces;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import codingtonportal.model.domain.Event;
 
@@ -13,8 +14,30 @@ public interface EventDAO {
 	 * @throws ClassNotFoundException
 	 */
 	public boolean insertEvent(Event event) throws IOException, ClassNotFoundException;
-	public boolean deleteEvent(Event event) throws IOException, ClassNotFoundException;
-	public boolean updateEvent(Event event) throws IOException, ClassNotFoundException;
-	public boolean selectEvent () throws IOException, ClassNotFoundException;
+	/**
+	 * The administrator inserts a new event.
+	 * @param event
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	
+	public boolean deleteEvent(Event event) throws IOException, ClassNotFoundException;
+	/**
+	 * The administrator deletes events from the list of events.
+	 * @param event
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	
+	public boolean updateEvent(Event event) throws IOException, ClassNotFoundException;
+	public ArrayList<Event> selectEvent () throws IOException, ClassNotFoundException;
+	//public ArrayList <Event> selectEvent() throws IOException, ClassNotFoundException;
+	
+		/**
+		 * The administrator selects a event.
+		 */
+		//public boolean updateSeatsAvailable(Event event) throws ClassNotFoundException, IOException;
+
 }
